5f88336c1f2a12eb16eda8b30e744ab70f357aae
ldhwaddell <ldhwaddell@gmail.com>
2024-03-30 21:57:53 -0300

    fix config file

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -16,9 +16,8 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
-    darkMode: "class",
-    plugins: [nextui()],
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
