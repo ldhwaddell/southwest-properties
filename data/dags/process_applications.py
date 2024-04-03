c52a6c39cd6f1d506246e34a33698b7259ed2ecf
ldhwaddell <ldhwaddell@gmail.com>
2024-04-02 23:52:44 -0300

    cleanup

--- data/dags/process_applications.py
+++ data/dags/process_applications.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-# from airflow.operators.python import PythonOperator
 
 from modules.applications_tracking.applications import scrape
 from modules.database.database import Database
@@ -19,6 +18,7 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
+
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime(2024, 1, 1),
@@ -32,57 +32,35 @@ with DAG(
         conn_id="pg_conn",
         sql="sql/applications_tables_schemas.sql",
     )
-    create_applications_tables.doc_md = "Creates the necessary 'applications' and 'applications_histories' if they do not exist"
+    create_applications_tables.doc_md = "Creates the necessary 'applications', 'scraped_applications', and 'applications_histories' tables if they do not exist"
 
-    @task()
+    @task(task_id="extract", retries=3)
     def extract():
-        """Extracts new application data by scraping."""
+        """Extracts new application data by scraping and upserts into the DB."""
+
         url = "https://www.halifax.ca/business/planning-development/applications"
-
-        return scrape(url)
-
-    @task()
-    def compare_and_load(scraped_data):
-        """Compares scraped application data against existing database records and performs necessary insertions/updates."""
         db = Database()
-        db.insert_applications(scraped_data)
+        applications = scrape(url)
+        db.upsert("scraped_applications", applications)
 
-        print(scraped_data)
-        # db_ops.update(scraped_data)
+    @task(task_id="compare_and_update", retries=3)
+    def compare_and_update():
+        """Compare scraped applications with those from DB, save any changes"""
+        db = Database()
+        scraped_applications = db.get_scraped_applications()
+        active_applications = db.get_active_applications()
+        db.update(scraped_applications, active_applications)
 
-    # Define task sequence
-    scraped_data = extract()
-    compare_and_load_task = compare_and_load(scraped_data=scraped_data)
+    drop_scraped_applications_table = SQLExecuteQueryOperator(
+        task_id="drop_scraped_applications_table",
+        conn_id="pg_conn",
+        sql="DROP TABLE scraped_applications;",
+    )
+    drop_scraped_applications_table.doc_md = "Drops the scraped_applications table."
 
-    # Setting task dependencies
-    create_applications_tables >> scraped_data >> compare_and_load_task
+    # Define the tasks
+    extract_task = extract()
+    compare_and_update_task = compare_and_update()
 
-    # populate_pet_table = PostgresOperator(
-    #     task_id="populate_pet_table",
-    #     sql="""
-    #         INSERT INTO pet (name, pet_type, birth_date, OWNER)
-    #         VALUES ( "Max", "Dog", "2018-07-05", "Jane");
-    #         INSERT INTO pet (name, pet_type, birth_date, OWNER)
-    #         VALUES ( "Susie", "Cat", "2019-05-01", "Phil");
-    #         INSERT INTO pet (name, pet_type, birth_date, OWNER)
-    #         VALUES ( "Lester", "Hamster", "2020-06-23", "Lily");
-    #         INSERT INTO pet (name, pet_type, birth_date, OWNER)
-    #         VALUES ( "Quincy", "Parrot", "2013-08-11", "Anne");
-    #         """,
-    #     postgres_conn_id="tutorial_pg_conn",
-    # )
-    # get_all_pets = PostgresOperator(
-    #     task_id="get_all_pets",
-    #     sql="SELECT * FROM pet;",
-    #     postgres_conn_id="tutorial_pg_conn",
-    # )
-
-    # get_birth_date = PostgresOperator(
-    #     task_id="get_birth_date",
-    #     sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
-    #     parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
-    #     hook_params={"options": "-c statement_timeout=3000ms"},
-    #     postgres_conn_id="tutorial_pg_conn",
-    # )
-
-  # >> populate_pet_table >> get_all_pets >> get_birth_date
+    # Set the task dependencies
+    create_applications_tables >> extract_task >> compare_and_update_task >> drop_scraped_applications_table
