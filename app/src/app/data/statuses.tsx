bb3f8d3701e8f41f46d70ce14b90f9a5ce70ab88
ldhwaddell <ldhwaddell@gmail.com>
2024-04-05 00:24:20 -0300

    add statuses list for filtering

---
+++ app/src/app/data/statuses.tsx
@@ -0,0 +1,14 @@
+import { CheckCircledIcon, CrossCircledIcon } from "@radix-ui/react-icons";
+
+export const statuses = [
+  {
+    value: "True",
+    label: "Active",
+    icon: CheckCircledIcon,
+  },
+  {
+    value: "False",
+    label: "Inactive",
+    icon: CrossCircledIcon,
+  },
+];
