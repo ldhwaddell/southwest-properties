69f2f003dfe4cf60c9cb3d6cc944a02a1cf51c75
ldhwaddell <ldhwaddell@gmail.com>
2024-04-06 16:23:34 -0300

    allow user to export as excel

--- app/src/components/export-excel.tsx
+++ app/src/components/export-excel.tsx
@@ -15,7 +15,7 @@ import { Switch } from "./ui/switch";
 import React, { useState } from "react";
 
 import { File } from "lucide-react";
-import { Table, Row } from "@tanstack/react-table";
+import { Table } from "@tanstack/react-table";
 import { Workbook } from "exceljs";
 import * as FileSaver from "file-saver";
 
@@ -24,10 +24,15 @@ interface ExportExcelProps<TData> {
   defaultFileName: string;
 }
 
-type UserModel = {
-  id: number;
-  name: string;
-  phone: string;
+const formatSnakeCaseToTitle = (text: string): string => {
+  const words = text.split("_");
+
+  // Capitalize the first letter of each word and join them with a space
+  const formattedText = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+  return formattedText;
 };
 
 export function ExportExcel<TData>({
@@ -41,56 +46,55 @@ export function ExportExcel<TData>({
     setFileName(event.target.value);
   };
 
-  // if (allColumnsChecked) {
-  //   console.log(table.getAllColumns());
-  // } else {
-  //   console.log(table.getVisibleFlatColumns());
-  // }
-
   const generateExcelFile = async () => {
     const fileType =
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
     const fileExtension = ".xlsx";
+
     const workbook = new Workbook();
     const worksheet = workbook.addWorksheet(defaultFileName);
+    let columns;
 
-    // Define column based on switch
+    // Define column based on switch position
     if (allColumnsChecked) {
-      console.log(table.getAllFlatColumns());
+      columns = table.getAllFlatColumns();
     } else {
-      console.log(table.getVisibleFlatColumns());
+      columns = table.getVisibleFlatColumns();
     }
 
-    // 3. Define columns and bind data fields to columns.
-    worksheet.columns = [
-      {
-        header: "ID", // Label of column's header.
-        key: "id", // The key to bind data field to column, see it in the {UserModel} above.
-        width: 10, // Optional, column width
-      },
-      { header: "Name", key: "name", width: 32 },
-      { header: "Phone", key: "phone", width: 32 },
-    ];
-    // 4. Add the data array to the worksheet
-    // worksheet.addRows(data);
-    // // you may also add an array of data as a row to while not using the header binding.
-    // // worksheet.addRow([1, 'John Doe', '0123456789']);
-    // const buffer = await workbook.xlsx.writeBuffer();
+    const sheetColumns = columns.map((column) => {
+      return {
+        header: formatSnakeCaseToTitle(column.id),
+        key: column.id,
+      };
+    });
 
-    // const excelBlob = new Blob([buffer], { type: fileType });
+    worksheet.columns = sheetColumns;
 
-    // FileSaver.saveAs(excelBlob, fileName + fileExtension);
+    const selectedRows = table.getSelectedRowModel();
+
+    // Match the desired columns with the fields from the rows
+    const dataForExport = selectedRows.rows.map((row) => {
+      const rowData: TData = row.original;
+      // Construct an object for the current row with keys and values matching the sheet columns
+      const rowObjectForSheet: Partial<TData> = {};
+
+      sheetColumns.forEach((column) => {
+        rowObjectForSheet[column.key as keyof TData] =
+          rowData[column.key as keyof TData];
+      });
+
+      return rowObjectForSheet;
+    });
+
+    worksheet.addRows(dataForExport);
+
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    const excelBlob = new Blob([buffer], { type: fileType });
+
+    FileSaver.saveAs(excelBlob, fileName + fileExtension);
   };
-
-  const users: UserModel[] = [
-    { id: 1, name: "John", phone: "123456789" },
-    { id: 2, name: "Anne", phone: "987654321" },
-    { id: 3, name: "Zack", phone: "123987456" },
-    { id: 4, name: "Jill", phone: "456123789" },
-    { id: 5, name: "Judy", phone: "789456123" },
-    { id: 6, name: "Jenny", phone: "123789456" },
-    { id: 7, name: "David", phone: "456789123" },
-  ];
 
   return (
     <Dialog>
