e3c3b488103a5d32cf3a5b00cb664ce0ecf4d778
ldhwaddell <ldhwaddell@gmail.com>
2024-04-10 17:04:26 -0300

    add sql for creating apartments tables

---
+++ data/dags/sql/apartments_dot_com_tables_schemas.sql
@@ -0,0 +1,46 @@
+/* The main listings table */
+CREATE TABLE IF NOT EXISTS apartments_dot_com_listings (
+    id VARCHAR(7) PRIMARY KEY,
+    available BOOLEAN,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    url VARCHAR,
+    address VARCHAR,
+    building VARCHAR,
+    monthly_rent VARCHAR,
+    bedrooms INTEGER,
+    bathrooms INTEGER,
+    square_feet FLOAT,
+    about TEXT,
+    description TEXT,
+    amenities TEXT,
+    fees TEXT
+);
+
+/* The intermediate listings the scraped entries get dumped into */
+CREATE TABLE IF NOT EXISTS scraped_apartments_dot_com_listings (
+    id VARCHAR(7) PRIMARY KEY,
+    available BOOLEAN,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    url VARCHAR,
+    address VARCHAR,
+    building VARCHAR,
+    monthly_rent VARCHAR,
+    bedrooms INTEGER,
+    bathrooms INTEGER,
+    square_feet FLOAT,
+    about TEXT,
+    description TEXT,
+    amenities TEXT,
+    fees TEXT
+);
+
+/* The table that holds changes make to listings in the main listings table */
+CREATE TABLE IF NOT EXISTS apartments_dot_com_listings_histories (
+    id SERIAL PRIMARY KEY,
+    listing_id VARCHAR(7) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    changed VARCHAR,
+    original TEXT,
+    updated TEXT,
+    FOREIGN KEY (listing_id) REFERENCES apartments_dot_com_listings(id) ON DELETE CASCADE
+);
