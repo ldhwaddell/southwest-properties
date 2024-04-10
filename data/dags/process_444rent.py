092e7d5f141b4d7eb87226166e93eeca591eef6e
ldhwaddell <ldhwaddell@gmail.com>
2024-04-10 11:31:50 -0300

    update for new DB logic

--- data/dags/process_444rent.py
+++ data/dags/process_444rent.py
@@ -6,6 +6,11 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 from modules.rental_tracking.fourfourfourrent import scrape
 from modules.database.database import Database
+from modules.database.models import (
+    FourFourFourRentListing,
+    FourFourFourRentListingHistory,
+    ScrapedFourFourFourRentListing,
+)
 
 DAG_ID = "444rent_listings_tracking_dag"
 
@@ -41,33 +46,41 @@ with DAG(
         url = "https://www.444rent.com/apartments.asp"
         db = Database()
         listings = scrape(url)
-        db.upsert("scraped_fourfourfourrent_listings", listings)
+        db.upsert(ScrapedFourFourFourRentListing, listings)
 
-    # @task(task_id="compare_and_update", retries=3)
-    # def compare_and_update():
-    #     """Compare scraped applications with those from DB, save any changes"""
-    #     db = Database()
-    #     scraped_applications = db.get_scraped_applications()
-    #     active_applications = db.get_active_applications()
-    #     db.update(scraped_applications, active_applications)
+    @task(task_id="compare_and_update", retries=3)
+    def compare_and_update():
+        """Compare scraped listings with those from DB, save any changes"""
+        db = Database()
+        scraped_listings = db.select_all(ScrapedFourFourFourRentListing)
+        available_listings = db.get(
+            FourFourFourRentListing, FourFourFourRentListing.available, True
+        )
+        db.update_records(
+            FourFourFourRentListing,
+            FourFourFourRentListingHistory,
+            FourFourFourRentListing.available,
+            scraped_listings,
+            available_listings,
+        )
 
-    # drop_scraped_applications_table = SQLExecuteQueryOperator(
-    #     task_id="drop_scraped_applications_table",
-    #     conn_id="pg_conn",
-    #     sql="DROP TABLE scraped_applications;",
-    # )
-    # drop_scraped_applications_table.doc_md = "Drops the scraped_applications table."
+    drop_scraped_applications_table = SQLExecuteQueryOperator(
+        task_id="drop_scraped_fourfourfourrent_listings_table",
+        conn_id="pg_conn",
+        sql="DROP TABLE scraped_fourfourfourrent_listings;",
+    )
+    drop_scraped_applications_table.doc_md = (
+        "Drops the scraped_fourfourfourrent_listings table."
+    )
 
     # Define the tasks
     extract_task = extract()
-    # compare_and_update_task = compare_and_update()
+    compare_and_update_task = compare_and_update()
 
     # Set the task dependencies
-    create_listings_tables >> extract_task
-    # (
-    #     create_applications_tables
-    #     >> extract_task
-    #     >> compare_and_update_task
-    #     >> drop_scraped_applications_table
-    # )
-    
+    (
+        create_listings_tables
+        >> extract_task
+        >> compare_and_update_task
+        >> drop_scraped_applications_table
+    )
