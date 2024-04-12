18cc57e2cca7fa1e6ed1eede0e3372c841190051
ldhwaddell <ldhwaddell@gmail.com>
2024-04-12 16:10:30 -0300

    fix spacing

--- app/src/components/rental-listings.tsx
+++ app/src/components/rental-listings.tsx
@@ -11,6 +11,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Separator } from "./ui/separator";
 
 import { ApartmentsDataTable } from "./apartments-table/data-table";
 import { columns as apartmentsColumns } from "./apartments-table/columns";
@@ -38,29 +39,34 @@ export function RentalListings({
 
   return (
     <>
-      <Select
-        defaultValue={listingSource}
-        value={listingSource}
-        onValueChange={handleListingSourceChange}
-      >
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a listing source" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Sources</SelectLabel>
-            <SelectItem value="444rent">444rent.com</SelectItem>
-            <SelectItem value="apartments">apartments.com</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-        {listingSource === "444rent" && <div>wooo</div>}
-        {listingSource === "apartments" && (
-          <ApartmentsDataTable
-            columns={apartmentsColumns}
-            data={apartments_dot_com_listings}
-          />
-        )}
+      <div className="mb-3 flex items-center gap-2">
+        <span className="whitespace-nowrap">Select a listing provider:</span>
+        <Select
+          defaultValue={listingSource}
+          value={listingSource}
+          onValueChange={handleListingSourceChange}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a listing source" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Sources</SelectLabel>
+              <SelectItem value="444rent">444rent.com</SelectItem>
+              <SelectItem value="apartments">apartments.com</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <Separator />
+
+      {listingSource === "444rent" && <div>wooo</div>}
+      {listingSource === "apartments" && (
+        <ApartmentsDataTable
+          columns={apartmentsColumns}
+          data={apartments_dot_com_listings}
+        />
+      )}
     </>
   );
 }
