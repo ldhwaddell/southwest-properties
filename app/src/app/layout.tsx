da45e7b3db631ad2e373866c1b52003aba8e1357
ldhwaddell <ldhwaddell@gmail.com>
2024-04-13 22:04:38 -0300

    add vercel speed insights

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
@@ -37,6 +38,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
+          <SpeedInsights />
         </body>
       </html>
     </>
