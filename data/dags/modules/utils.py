6c7b2be6e3b7e2d7777f18020abfb994c0df69ed
ldhwaddell <ldhwaddell@gmail.com>
2024-04-17 21:51:17 -0300

    add clean whitespace function

--- data/dags/modules/utils.py
+++ data/dags/modules/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import re
 
 
 def generate_hash(s: str) -> str:
@@ -6,3 +7,9 @@ def generate_hash(s: str) -> str:
     # Encode the string to bytes
     hasher.update(s.encode("utf-8"))
     return hasher.hexdigest()
+
+
+def clean_whitespace(text: str) -> str:
+    """Replace one or more whitespace characters (including non-breaking spaces) with a single space"""
+    if text is not None:
+        return re.sub(r"\s+", " ", text)
