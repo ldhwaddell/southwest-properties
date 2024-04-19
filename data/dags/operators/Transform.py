92e79799eda7c93992df57dded2cfe3e6aa76473
ldhwaddell <ldhwaddell@gmail.com>
2024-04-19 17:22:01 -0300

    update to save results to redis

--- data/dags/operators/Transform.py
+++ data/dags/operators/Transform.py
@@ -5,6 +5,7 @@ from typing import Dict
 
 from airflow.models import BaseOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.redis.hooks.redis import RedisHook
 
 
 class TransformOperator(BaseOperator):
@@ -14,6 +15,8 @@ class TransformOperator(BaseOperator):
         transform_fn_name,
         transform_fn_file,
         pg_conn_id,
+        redis_conn_id,
+        redis_key,
         *args,
         **kwargs,
     ):
@@ -22,8 +25,11 @@ class TransformOperator(BaseOperator):
         self.transform_fn_name = transform_fn_name
         self.transform_fn_file = transform_fn_file
         self.pg_conn_id = pg_conn_id
+        self.redis_conn_id = redis_conn_id
+        self.redis_key = redis_key
 
     def execute(self, context):
+        # Get the transform function
         transform_function = getattr(
             importlib.import_module(self.transform_fn_file), self.transform_fn_name
         )
@@ -37,4 +43,10 @@ class TransformOperator(BaseOperator):
         if not record:
             raise ValueError(f"No data found in {self.raw_data_table}")
 
+        # Build the redis connection
+        redis_hook = RedisHook(redis_conn_id=self.redis_conn_id).get_conn()
+
         result = transform_function(record)
+
+        # Save to redis
+        redis_hook.set(self.redis_key, json.dumps(result))
