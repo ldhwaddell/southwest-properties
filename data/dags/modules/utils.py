0ee69dbfc4b3c00b5f34d1a37e8bc1352c931a24
ldhwaddell <ldhwaddell@gmail.com>
2024-04-19 17:21:23 -0300

    add load from redis util

--- data/dags/modules/utils.py
+++ data/dags/modules/utils.py
@@ -1,6 +1,8 @@
 import hashlib
 import re
 
+from airflow.providers.redis.hooks.redis import RedisHook
+
 
 def generate_hash(s: str) -> str:
     hasher = hashlib.sha256()
@@ -13,3 +15,20 @@ def clean_whitespace(text: str) -> str:
     """Replace one or more whitespace characters (including non-breaking spaces) with a single space"""
     if text is not None:
         return re.sub(r"\s+", " ", text)
+
+
+def load_from_redis(conn_id: str, key: str) -> bytes:
+    redis_hook = RedisHook(redis_conn_id=conn_id).get_conn()
+
+    with redis_hook.pipeline() as pipe:
+        pipe.get(key)
+        pipe.delete(key)
+        result = pipe.execute()
+
+    # [0] is get, [1] is delete status
+    value = result[0]
+
+    if value is None:
+        raise ValueError(f"No data found in Redis for key: {key}")
+
+    return value
