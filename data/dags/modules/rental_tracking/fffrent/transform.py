84dc27b2dffbf70498e3b67fd4627c65bdc205d5
ldhwaddell <ldhwaddell@gmail.com>
2024-04-29 17:41:43 -0300

    update transform function

--- data/dags/modules/rental_tracking/fffrent/transform.py
+++ data/dags/modules/rental_tracking/fffrent/transform.py
@@ -1,9 +1,9 @@
+import json
 import logging
-
 import pendulum
+import re
 
-from typing import Tuple, List, Dict, Union
-
+from typing import Tuple, List, Dict
 import pendulum.exceptions
 
 from modules.utils import generate_hash
@@ -54,54 +54,85 @@ def get_building(building: str) -> str:
     else:
         return building
 
-        #     # Create an id using the building name and unit:
-        # id_str = (
-        #     row_data["building"] + row_data["unit"]
-        #     if row_data["unit"]
-        #     else row_data["building"]
-        # )
-        # row_data["id"] = generate_hash(id_str)
-
 
 def parse_unit(unit: str) -> int:
 
     cleaned_unit = unit.split("-")[-1]
 
-    return int(cleaned_unit)
+    return parse_number(cleaned_unit, int)
 
 
-def parse_rooms(): ...
+def parse_square_feet(square_feet: str) -> float:
+
+    cleaned_str = square_feet.replace("ft2", "")
+
+    return parse_number(cleaned_str, float)
 
 
-def parse_float(s: str) -> Union[float, str]:
-
+def parse_number(s: str, type):
     try:
-        cleaned_str = "".join(filter(str.isdigit, s))
-        return float(cleaned_str)
+        cleaned_str = "".join(filter(lambda x: x.isdigit() or x == ".", s))
+
+        return type(cleaned_str)
     except ValueError as e:
         logging.error(f"Error ocurred parsing float: {e}")
         return s
 
 
-def parse_bathroom(): ...
-
-
-def clean_text(): ...
-
-
-def parse_date(date: str) -> ...:
+def parse_date(date: str):
 
     if date == "Now":
-        return pendulum.now().date()
+        return pendulum.now().date().to_date_string()
 
     format_string = "MMM D, YYYY"
 
     try:
         parsed_date = pendulum.from_format(date, format_string)
-        return parsed_date.date()
+        return parsed_date.date().to_date_string()
     except Exception as e:
         logging.error(f"Error ocurred parsing date: {e}")
         return date
+
+
+def parse_bedrooms(bedrooms: str):
+    if bedrooms == "Bachelor":
+        return 1
+    else:
+        return parse_number(bedrooms, int)
+
+
+def parse_bathrooms(bathrooms: str):
+    if bathrooms == "Bachelor":
+        return 1
+
+    match = re.search(r"(\d+)\s*Baths?", bathrooms, re.IGNORECASE)
+    return parse_number(match.group(1), int)
+
+
+def parse_den(rooms: str) -> bool:
+    return "DEN" in rooms
+
+
+def clean_leasing_info(info: Dict) -> Dict:
+    for key, val in info.items():
+
+        if val:
+            cleaned_val = [s.replace("\xa0", " ") for s in val]
+            info[key] = cleaned_val
+
+        if key == "deposit":
+            cleaned_deposit = parse_number(info[key][0], float)
+            info[key][0] = cleaned_deposit
+
+    return info
+
+
+def clean_suite_info(info: Dict) -> Dict:
+    for key, val in info.items():
+        if val:
+            cleaned_val = [s.replace("\xa0", " ") for s in val]
+            info[key] = cleaned_val
+    return info
 
 
 def transform(record: Tuple) -> List[Dict]:
@@ -128,26 +159,33 @@ def transform(record: Tuple) -> List[Dict]:
         listing["unit"] = parse_unit(listing["unit"])
 
         # Turn the square feet to number
-        listing["square_feet"] = parse_float(listing["square_feet"])
+        listing["square_feet"] = parse_square_feet(listing["square_feet"])
 
         # Available date?
         listing["available_date"] = parse_date(listing["available_date"])
 
         # Turn the price to number
-        listing["price"] = parse_float(listing["price"])
+        listing["price"] = parse_number(listing["price"], float)
 
-        # bedrooms
+        # Get the number of bedrooms
+        listing["bedrooms"] = parse_bedrooms(listing["bedrooms"])
 
-        # bathrooms
+        # Check if listing has den
+        listing["den"] = parse_den(listing["bathrooms"])
 
-        # den
+        # Get number of bathrooms
+        listing["bathrooms"] = parse_bathrooms(listing["bathrooms"])
 
-        # clean leasing_info, description_info, building_info, suite_info
+        # Description info does not need to be cleaned
+        listing["leasing_info"] = clean_leasing_info(listing["leasing_info"])
+        listing["description_info"] = listing["description_info"].replace("\xa0", " ")
+        listing["suite_info"] = clean_suite_info(listing["suite_info"])
 
     return data
 
 
 if __name__ == "__main__":
+    conn = psycopg2.connect(user="airflow", password="southwest2024", host="localhost")
     cursor = conn.cursor()
     sql = f"SELECT * FROM scraped_fffrent_listings ORDER BY created_at DESC LIMIT 1"
     cursor.execute(sql)
